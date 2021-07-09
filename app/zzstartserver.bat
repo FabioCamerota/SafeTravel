5281c8074ecfa7c009d2312d59aa93bd80d057c0
laaners <kirbyalessio@yahoo.it>
2021-07-09 23:35:36 +0200

    less log

--- app/zzstartserver.bat
+++ app/zzstartserver.bat
@@ -2,6 +2,10 @@
 ::npm install https --save-prod
 ::PUT http://admin:admin@127.0.0.1:5984/users
 
-docker-compose build
-docker-compose up
+::docker-compose build
+::docker-compose up
 ::docker-compose down
+
+docker-compose build
+docker-compose up -d
+docker-compose logs -f nodeserver
